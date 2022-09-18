f8d81ce20aca696850315eca755e84ecb9e8d921
tamasakti <gumilangtamasakti@gmail.com>
2022-09-19 02:43:32 +0700

    update revisi promises js

--- promise.js
+++ promise.js
@@ -2,11 +2,12 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emosiPenonton) => {
-  
-  let result = 0;
+  await new Promise((emosiPenonton) => setTimeout(emosiPenonton, 200));
   const penontonTheaterIXX = await promiseTheaterIXX(emosiPenonton);
   const penontonTheaterVGC = await promiseTheaterVGC(emosiPenonton);
 
+return new Promise((resolve) => {
+  let result = 0;
   for(const emosi of penontonTheaterIXX){
     if(emosiPenonton === emosi.hasil){
       result++;
@@ -14,13 +15,14 @@ const promiseOutput = async (emosiPenonton) => {
   }
 
   for(const emosi of penontonTheaterVGC){
-    if(emosiPenonton === emosi.hasil ){
-      result++;
+    if(emosiPenonton === emosi.hasil){
+      result++
     }
   }
+  return resolve(result);
+}) 
 
-  return result;
-};
+}
 
 
 module.exports = {
