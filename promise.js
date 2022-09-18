437e30e66727aef34048a2b276f8624dc33cef30
tamasakti <gumilangtamasakti@gmail.com>
2022-09-19 01:56:17 +0700

    membuat fungsi promises di js

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosiPenonton) => {
+  
+  let result = 0;
+  const penontonTheaterIXX = await promiseTheaterIXX(emosiPenonton);
+  const penontonTheaterVGC = await promiseTheaterVGC(emosiPenonton);
+
+  for(const emosi of penontonTheaterIXX){
+    if(emosiPenonton === emosi.hasil){
+      result++;
+    }
+  }
+
+  for(const emosi of penontonTheaterVGC){
+    if(emosiPenonton === emosi.hasil ){
+      result++;
+    }
+  }
+
+  return result;
+};
+
 
 module.exports = {
   promiseOutput,
